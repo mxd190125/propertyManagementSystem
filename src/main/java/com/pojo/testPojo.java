da2697f391be737fc3f0fed91eb65e3b09115fd7
mxd190125 <2860850965@qq.com>
2020-03-19 19:04:22 +0800

    测试添加java目录到master_马雪冬(第三次)

---
+++ src/main/java/com/pojo/testPojo.java
@@ -0,0 +1,13 @@
+package com.pojo;
+
+/**
+ * @ClassName testPojo
+ * @Version 1.0
+ * @Author dell
+ * @Date 2020/3/19 19:03
+ * @Description TODO
+ * Modification User:
+ * Modification Date:
+ */
+public class testPojo {
+}
---
+++ src/main/java/com/service/impl/VerifyAccessServiceImpl.java
@@ -0,0 +1,13 @@
+package com.service.impl;
+
+/**
+ * @ClassName VerifyAccessServiceImpl
+ * @Version 1.0
+ * @Author dell
+ * @Date 2020/3/19 19:02
+ * @Description TODO
+ * Modification User:
+ * Modification Date:
+ */
+public class VerifyAccessServiceImpl {
+}
