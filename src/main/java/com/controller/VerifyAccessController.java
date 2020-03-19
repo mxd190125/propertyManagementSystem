2a18936daee37c16a5c0f4a781b201455b1ff290
mxd190125 <2860850965@qq.com>
2020-03-19 18:58:07 +0800

    添加java目录_马雪冬(第二次)

---
+++ src/main/java/com/controller/VerifyAccessController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+/**
+ * @ClassName VerifyAccessController
+ * @Version 1.0
+ * @Author dell
+ * @Date 2020/3/19 18:56
+ * @Description TODO
+ * Modification User:
+ * Modification Date:
+ */
+public class VerifyAccessController {
+}
